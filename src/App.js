5aec38857cd50036b85072061c3ddb187af57345
Erik De Jesus <erikdcodes@gmail.com>
2021-03-28 19:32:31 -0700

    setup routing and initial style resets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import StylesTest from "./Components/StylesTest";
+
 function App() {
-  return <div className="App">new app</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <div>
+            <h1>Home</h1>
+          </div>
+        </Route>
+        <Route path="/styles" exact>
+          <StylesTest />
+        </Route>
+        <Route path="*">
+          <div>
+            <h1>404 Not Found</h1>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/Components/StylesTest.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const StylesTest = () => {
+  return (
+    <div>
+      <h1>Test</h1>
+      <h2>Test</h2>
+      <h3>Test</h3>
+      <h4>Test</h4>
+      <h5>Test</h5>
+      <h6>Test</h6>
+      <p>
+        Paragraph 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+        enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+        officia deserunt mollit anim id est laborum. <a href="#">Link Here</a>
+      </p>
+      <p>
+        Paragraph 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+        enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+        officia deserunt mollit anim id est laborum. <a href="#">Link Here</a>
+      </p>
+      <a href="#">Link Here</a>
+      <button>Button</button>
+      <button>Button</button>
+      <button>Button</button>
+      <ul>
+        <li>
+          Unordered List 1 <a href="#">Link Here</a>
+        </li>
+        <li>
+          Unordered List 2<a href="#">Link Here</a>
+        </li>
+        <li>
+          Unordered List 3<a href="#">Link Here</a>
+        </li>
+      </ul>
+      <ol>
+        <li>Ordered List 1</li>
+        <li>Ordered List 2</li>
+        <li>Ordered List 3</li>
+      </ol>
+    </div>
+  );
+};
+
+export default StylesTest;
