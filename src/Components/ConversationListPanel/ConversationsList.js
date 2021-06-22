5c16288c2ace5286ee70306df15094f48e16e291
Erik De Jesus <erikdcodes@gmail.com>
2021-06-21 19:50:02 -0700

    add axios

--- src/Components/ConversationListPanel/ConversationsList.js
+++ src/Components/ConversationListPanel/ConversationsList.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { styleVariables } from "../../GlobalStyles/StyleVariables";
 import ConversationItem from "../ConversationListPanel/ConversationItem";
--- src/Store/UIState.js
+++ src/Store/UIState.js
@@ -12,5 +12,10 @@ export const loggedInState = atom({
 
 export const userDataState = atom({
   key: "userDataState",
-  default: null, //object
+  default: {
+    _id: "60c6b2f3ba0a6e08494df73b",
+    userTwilioPhone: "+19512277675",
+    userAuth0ID: "1234",
+    __v: 0,
+  }, //object
 });
