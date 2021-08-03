f14c14833d2c00bb0070c23d8656394978ff751a
Erik De Jesus <erikdcodes@gmail.com>
2021-08-03 13:56:20 -0700

    .

--- src/Components/ActionsListPanel/ActionItem.js
+++ src/Components/ActionsListPanel/ActionItem.js
@@ -28,7 +28,7 @@ const relativeDue = (dueDate) => {
     sameDay: "[Today at] h:mm A", // The same day ( Today at 2:30 AM )
     nextDay: "[Tomorrow at] h:mm A", // The next day ( Tomorrow at 2:30 AM )
     nextWeek: "[This] dddd", // The next week ( Sunday at 2:30 AM )
-    lastDay: "[Yesterday]", // The day before ( Yesterday at 2:30 AM )
+    lastDay: "[Yesterday at] h:mm A", // The day before ( Yesterday at 2:30 AM )
     lastWeek: "[Last] dddd MM/DD/YYYY", // Last week ( Last Monday at 2:30 AM )
     sameElse: "MM/DD/YYYY [at] h:mm A", // Everything else ( 7/10/2011 )
   });
