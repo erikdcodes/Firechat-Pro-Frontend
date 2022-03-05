0bef742b38894641f54ed412012830650a7ff272
Erik De Jesus <erikdcodes@gmail.com>
2022-03-05 08:34:32 -0800

    update styling

--- src/Components/PageComponents/Section.js
+++ src/Components/PageComponents/Section.js
@@ -1,15 +1,88 @@
 import React from "react";
 import styled from "styled-components";
 import { styleVariables } from "../../GlobalStyles/StyleVariables.js";
-const Section = () => {
-  return <Wrapper>Section</Wrapper>;
+
+const Section = ({ title, description, img, orientation }) => {
+  return (
+    <>
+      {orientation !== "standard" ? (
+        <Wrapper>
+          <div className="container">
+            <div className="left">
+              <h2 className="title">{title}</h2>
+              <p className="description">{description}</p>
+            </div>
+            <div className="right">
+              <img
+                className="hero-img"
+                src={img}
+                alt="animated example of sms"
+              />
+            </div>
+          </div>
+        </Wrapper>
+      ) : (
+        <Wrapper className="flipped">
+          <div className="container">
+            <div className="right">
+              <img
+                className="hero-img"
+                src={img}
+                alt="animated example of sms"
+              />
+            </div>
+            <div className="left  ">
+              <h2 className="title">{title}</h2>
+              <p className="description">{description}</p>
+            </div>
+          </div>
+        </Wrapper>
+      )}
+    </>
+  );
 };
 
 export default Section;
 
 const Wrapper = styled.section`
-  outline: 1px solid red;
-  padding: 30px;
+  padding-block: 100px;
   background: ${styleVariables.bgColor1};
-  display: flex;
+
+  .container {
+    display: flex;
+    justify-content: space-evenly;
+    margin-inline: auto;
+    width: min(90%, 1140px);
+    gap: 45px;
+  }
+
+  &.flipped {
+    background: ${styleVariables.accentColorBlue};
+    color: #fff;
+    .title,
+    .description {
+      color: #fff;
+    }
+  }
+
+  .left {
+    flex: 1 0 25%;
+  }
+  .right {
+    flex: 1 0 75%;
+  }
+
+  .hero-img {
+    width: 100%;
+    box-shadow: 0px 1px 25px rgba(121, 120, 120, 0.603);
+  }
+  .title {
+    color: ${styleVariables.primaryTextColor};
+    font-size: ${styleVariables.heading1Size};
+  }
+
+  .description {
+    font-size: ${styleVariables.textSize};
+    color: ${styleVariables.secondaryTextColor};
+  }
 `;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,6 +6,23 @@ import useCurrentUser from "../Hooks/useCurrentUser";
 import headerBg from "../Images/bg-pattern-header.svg";
 import Section from "../Components/PageComponents/Section";
 
+const sections = [
+  {
+    title: "Send and Receive Real Time SMS Text Messages",
+    description:
+      "Business text messaging is proven to generate stronger engagement from potential clients.",
+    img: "/sms-example.gif",
+    orientation: "flipped",
+  },
+  {
+    title: "Supercharge your follow-up work flow",
+    description:
+      "Only focus on the single most important task at hand for each client. After you've completed that action, set the next one.  No more dead to-do lists filled with expired items",
+    img: "/action-view-example.gif",
+    orientation: "standard",
+  },
+];
+
 const Home = () => {
   const [currentUser] = useCurrentUser();
   const history = useHistory();
@@ -29,9 +46,27 @@ const Home = () => {
               </button>
             )}
           </div>
-          <div className="right"></div>
+          <div className="right">
+            <img src="/hero-img-2.jpeg" alt="" />
+          </div>
         </header>
-        <Section />
+        {sections.map((section, i) => {
+          return (
+            <Section
+              key={i + section.title}
+              title={section.title}
+              description={section.description}
+              img={section.img}
+              orientation={section.orientation}
+            />
+          );
+        })}
+        <footer>
+          <div className="container">
+            <p>© 2022 Erik De Jesus </p>
+            <a href="https://github.com/erikdcodes">@Github</a>
+          </div>
+        </footer>
       </Wrapper>
     </AppLayout>
   );
@@ -41,7 +76,9 @@ const Wrapper = styled.div`
   header {
     height: calc(100vh - 80px);
     background: url(${headerBg}) no-repeat fixed right top;
-    padding: 30px;
+    margin-inline: auto;
+    width: min(90%, 1440px);
+    padding-block: 100px;
     display: flex;
 
     .title {
@@ -59,10 +96,14 @@ const Wrapper = styled.div`
     }
 
     .left {
-      flex-basis: 50%;
+      flex-basis: 90%;
       display: flex;
       flex-direction: column;
       justify-content: center;
+    }
+
+    .right img {
+      width: 100%;
     }
 
     button {
@@ -70,6 +111,18 @@ const Wrapper = styled.div`
       max-width: 400px;
       font-size: 24px;
     }
+  }
+  footer {
+    background: #4a4a52;
+    color: #fff;
+    padding-block: 30px;
+  }
+  .container {
+    margin-inline: auto;
+    width: min(90%, 1140px);
+    display: flex;
+    gap: 15px;
+    justify-content: center;
   }
 `;
 
